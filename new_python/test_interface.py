17846b9a000cfcd3797f940726aa473afe1f7bc2
sameer <sameer@ices.utexas.edu>
2019-02-14 18:05:31 -0600

    all data tests written (for now), additional tests can be added here

--- new_python/test_interface.py
+++ new_python/test_interface.py
@@ -1,26 +1,81 @@
 import PyData
 import unittest
+import os
 
 class PyDataTesting(unittest.TestCase):
     # test get/set
+    def test_getset(self):
+        pd = PyData.PyData(7,8)
+
+        # set value 
+        pd.setvalue(2,4,2.5) # choosing numbers representable in base 2
+        pd.setvalue(4,2,4.25)
+
+        # test get value
+        self.assertEqual(pd.getvalue(2,4), 2.5)
+        self.assertEqual(pd.getvalue(4,2), 4.25)
 
     # test initialization
     def test_initialization(self):
-        pd = PyData.PyData(10,15);
-        self.assertEqual(pd.row(),10);
-        self.assertEqual(pd.col(),15);
+        pd = PyData.PyData(10,15)
+        self.assertEqual(pd.row(),10)
+        self.assertEqual(pd.col(),15)
 
     # test null init
     def test_null(self):
-        pd = PyData.PyData();
-        self.assertEqual(pd.row(),0);
-        self.assertEqual(pd.col(),0);
+        pd = PyData.PyData()
+        self.assertEqual(pd.row(),0)
+        self.assertEqual(pd.col(),0)
 
 
     # test randomizer
+    def test_rand(self):
+        # initialize
+        m = 100
+        pd = PyData.PyData(m,2)
+
+        # call rand
+        pd.rand(0.5,1.5)
+
+
+        # sum a column and check falls within bounds
+        col_sum = 0.0
+        for i in range(m):
+            col_sum += pd.getvalue(i,0)
+
+        # divide by m
+        col_sum = col_sum/m;
+
+        # should be within 0.25,0.75
+        self.assertTrue(col_sum > 0.9 and col_sum < 1.1,msg='Randomize failed, sum is {}'.format(col_sum) )
 
 
     # test save/load
+    def test_saveload(self):
+        m = 5
+        # initialize
+        pd = PyData.PyData(m,m)
+
+        # set value 
+        pd.setvalue(2,4,2.5) # choosing numbers representable in base 2
+        pd.setvalue(4,2,4.25)
+
+        # write to file
+        filenm = './tmp_test_data.bin'
+        pd.write(filenm)
+
+        # load
+        pd2 = PyData.PyData(m,m)
+        pd2.read(m,m,filenm)
+        os.remove(filenm)
+
+
+        # assert equal at every position
+        for i in range(m):
+            for j in range(m):
+                self.assertEqual( pd.getvalue(i,j), pd2.getvalue(i,j) )
+
+
 
 if __name__=='__main__':
     unittest.main()
