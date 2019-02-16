8006a873f3c1e545ca7d3ac042bafcf1acda3e3b
dialecticDolt <will.ruys@gmail.com>
2019-02-15 18:06:57 -0600

    Expect config and splitter to to be broken in this committ

--- new_python/pyconfig.pyx
+++ new_python/pyconfig.pyx
@@ -2,9 +2,19 @@ from config cimport *
 
 cdef class PyConfig:
 	cdef Configuration[float] c_config
-	
-	def __cinit__(self, DistanceMetric metric_type, int problem_size, int leaf_node_size, int neighbor_size, int maximum_rank, float tolerance, float budget, bool secure_accuracy):
-		self.c_config = Configuration[float](metric_type, problem_size, leaf_node_size, neighbor_size, maximum_rank, tolerance, budget, secure_accuracy)
+	string metric_type
+
+	def __cinit__(self, string metric_type, int problem_size, int leaf_node_size, int neighbor_size, int maximum_rank, float tolerance, float budget, bool secure_accuracy):
+		self.metric_type = metric_type
+		if(metric_type == "GEOMETRY_DISTANCE"):
+			m = 0
+		elif(metric_type == "KERNEL_DISTANCE"):
+			m = 1
+		elif(metric_type == "ANGLE_DISTANCE"):
+			m = 2
+		elif(metric_type == "USER_DISTANCE"):
+			m = 3
+		self.c_config = Configuration[float](m, problem_size, leaf_node_size, neighbor_size, maximum_rank, tolerance, budget, secure_accuracy)
 	
 	def getMetricType(self):
 		return self.c_config.MetricType()
---
+++ new_python/split.pyx
@@ -0,0 +1,13 @@
+from splitter cimport *
+
+cdef class centerSplit:
+	cdef centersplit c_csplit
+	def __cinit__(self, SPDMATRIX* K):
+		self.c_csplit =  centersplit(K);
+
+cdef class randomSplit:
+	cdef randomsplit c_rsplit
+	def __cinit(self, SPDMATRIX*K):
+		self.c_rsplit = centersplit(K);
+
+
