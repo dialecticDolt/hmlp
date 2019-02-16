9d5fa93a25aef2449dae43e52e625e54f4e92985
sameer <sameer@ices.utexas.edu>
2019-02-15 18:14:53 -0600

    changed class attribute to be pointer to c++ object rather than direct

--- new_python/PyData.pyx
+++ new_python/PyData.pyx
@@ -1,10 +1,14 @@
 from Data cimport Data
+#cimport as numpy as np
+from cython.operator cimport dereference as deref
+
+
 
 cdef class PyData:
-    cdef Data[float] c_data
+    cdef Data[float]* c_data
 	
     def __cinit__(self,size_t m = 0,size_t n = 0):
-        self.c_data = Data[float](m,n)
+        self.c_data = new Data[float](m,n)
 	
     cpdef read(self, size_t m, size_t n, str filename):
         self.c_data.read(m, n,filename.encode())
@@ -13,7 +17,7 @@ cdef class PyData:
         self.c_data.write(filename.encode())
 
     cpdef row(self):
-        return self.c_data.row()
+        return self.c_data[0].row()
     
     cpdef col(self):
         return self.c_data.col()
@@ -32,3 +36,18 @@ cdef class PyData:
 
     # TODO pass in numpy and make a data object?? try with [:]
     # not sure it is necessary, so going to leave this for later
+
+    # TODO access submatrix through inputting numpy vectors
+    #cpdef submatrix(self,np.ndarray[np.int, ndim=1] I not None,
+    #        np.ndarray[np.int,ndim=1] J not None):
+
+    #    # get sizes, initialize new PyData
+    #    size_t ni = I.size()
+    #    size_t nj = J.size()
+    #    PyData sub = PyData(ni,nj)
+
+    #    # call c_data's sub func
+    #    sub.c_data = self.c_data(I,J)
+
+    #    # return sub
+    #    return sub
