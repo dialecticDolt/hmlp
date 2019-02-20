03ea4ceafd1a2df2a6477d9d0ece16915cfe6253
sameer <sameer@ices.utexas.edu>
2019-02-20 12:47:58 -0600

    added foundations for kernel matrix class (kernel_s struct and kernel_type enum)

--- new_python/PyMatrix.pyx
+++ new_python/PyMatrix.pyx
@@ -1,4 +1,4 @@
-from Matrix cimport SPDMatrix
+from Matrix cimport * 
 
 cdef class PySPDMatrix:
     cdef SPDMatrix[float]* c_matrix
@@ -23,4 +23,48 @@ cdef class PySPDMatrix:
         return self.c_matrix[0](m,n)
 
 
+cdef class PyKernel:
+    cdef kernel_s[float,float]* c_kernel
+
+    # constructor 
+    def __cinit__(self,str kstring="GAUSSIAN"):
+       self.c_kernel = new kernel_s[float,float]()
+       k_enum = PyKernel.GetKernelTypeEnum(kstring)
+       self.c_kernel.SetKernelType(k_enum)
+
+
+    # static method for handling enum
+    @staticmethod
+    def GetKernelTypeEnum(str kstring):
+        if(kstring == "GAUSSIAN"):
+            m = int(0)
+        elif(kstring == "SIGMOID"):
+            m = int(1)
+        elif(kstring == "POLYNOMIAL"):
+            m = int(2)
+        elif(kstring == "LAPLACE"):
+            m = int(3)
+        elif(kstring == "GAUSSIAN_VAR_BANDWIDTH"):
+            m = int(4)
+        elif(kstring == "TANH"):
+            m = int(5)
+        elif(kstring == "QUARTIC"):
+            m = int(6)
+        elif(kstring == "MULTIQUADRATIC"):
+            m = int(7)
+        elif(kstring == "EPANECHNIKOV"):
+            m = int(8)
+        elif(kstring == "USER_DEFINE"):
+            m = int(9)
+
+        return m
+    
+
+#cdef class PyKernelMatrix:
+#    cdef KernelMatrix[float]* c_matrix
+#    
+#    # constructor 
+#    def __cinit__(self, size_t m = 0, size_t n = 0, str source_file = None, str target_file = None):
+#        # read in sources, targets if availaible
+#    
 
