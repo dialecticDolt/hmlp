ab62776d33ff176fea60ab7c38c24b07d332b883
sameer <sameer@ices.utexas.edu>
2019-04-02 19:23:55 -0500

    added fixes to some error scripts, also pushed cpp file

--- example/analyze_matvec_ordering.py
+++ example/analyze_matvec_ordering.py
@@ -1,30 +1,30 @@
 import numpy as np
 
-1_processor_data = np.load('temp.data_1p')
-1_processor_gids = np.load('temp.gids_1p')
+processor_1_data = np.fromfile('temp.data_p1',dtype = 'float32')
+processor_1_gids = np.fromfile('temp.gids_p1',dtype = 'int32') 
 
-2_processor_data = np.load('temp.data_2p')
-2_processor_gids = np.load('temp.gids_2p')
+processor_2_data = np.fromfile('temp.data_p2',dtype = 'float32')
+processor_2_gids = np.fromfile('temp.gids_p2',dtype = 'int32') 
 
-4_processor_data = np.load('temp.data_4p')
-4_processor_gids = np.load('temp.gids_4p')
+processor_4_data = np.fromfile('temp.data_p4',dtype = 'float32')
+processor_4_gids = np.fromfile('temp.gids_p4',dtype = 'int32')
 
 index_on_1 = 0
-table = np.zeros( [4, len(1_processor_gids)] )
-for i in 1_processor_data:
-    index_on_2 = np.argmin(np.abs(2_processor_data - i))
-    index_on_4 = np.argmin(np.abs(4_processor_data - i))
+table = np.zeros( [4, len(processor_1_gids)] )
+for i in processor_1_data:
+    index_on_2 = np.argmin(np.abs(processor_2_data - i))
+    index_on_4 = np.argmin(np.abs(processor_4_data - i))
 
     table[0, index_on_1] = i
-    table[1, index_on_1] = 1_processor_gids[index_on_1]
-    table[2, index_on_1] = 2_processor_gids[index_on_2]
-    table[3, index_on_1] = 4_processor_gids[index_on_4]
+    table[1, index_on_1] = processor_1_gids[index_on_1]
+    table[2, index_on_1] = processor_2_gids[index_on_2]
+    table[3, index_on_1] = processor_4_gids[index_on_4]
 
 table = table[:, table[1, :].argsort()]
 table = table.T
 
 print("Data", "GID on 1P", "GID on 2P", "GID on 4P")
-for i in range(len(rids1)):
+for i in range(len(processor_1_gids)):
     print( table[i, 0], (int)(table[i, 1]), (int)(table[i, 2]), (int)(table[i, 3]) )
 
 
--- example/run_matvec_ordering_test.sh
+++ example/run_matvec_ordering_test.sh
@@ -1,12 +1,12 @@
-mpirun -n 1 ./readPoints
-cat temp.data_r0 > temp.data_p1
-cat temp.gids_r0 > temp.gids_p1
-mpirun -n 2 ./readPoints
-cat temp.data_r0 temp.data_r1 > temp.data_p2
-cat temp.gids_r0 temp.gids_r1 > temp.gids_p2
-mpirun -n 4 ./readPoints
-cat temp.data_r0 temp.data_r1 temp.data_r2 temp.data_r3 > temp.data_p4
-cat temp.gids_r0 temp.gids_r1 temp.gids_r2 temp.gids_r3 > temp.gids_p4
+mpirun -n 1 ./my_gofmm_test
+cat temp.data_r0.bin > temp.data_p1
+cat temp.gids_r0.bin > temp.gids_p1
+mpirun -n 2 ./my_gofmm_test
+cat temp.data_r0.bin temp.data_r1.bin > temp.data_p2
+cat temp.gids_r0.bin temp.gids_r1.bin > temp.gids_p2
+mpirun -n 4 ./my_gofmm_test
+cat temp.data_r0.bin temp.data_r1.bin temp.data_r2.bin temp.data_r3.bin > temp.data_p4
+cat temp.gids_r0.bin temp.gids_r1.bin temp.gids_r2.bin temp.gids_r3.bin > temp.gids_p4
 
 python analyze_matvec_ordering.py
 
